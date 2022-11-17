862c8d2b49e7b7c9bbf401a9e11fa5249cb7eb66
arw-wk <118310911+arw-wk@users.noreply.github.com>
2022-11-17 15:08:32 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,12 @@
-//
+// Overrides of global vars need to be pre-loaded
+window.EXPORT_URL = 'https://convert.diagrams.net/node/export';
+window.PLANT_URL = 'REPLACE_WITH_YOUR_PLANTUML_SERVER';
+window.DRAWIO_BASE_URL = 'https://arw-wk.github.io/abd/src/main/webapp/'; // Replace with path to base of deployment, e.g. https://www.example.com/folder
+window.DRAWIO_VIEWER_URL = 'https://arw-wk.github.io/abd/src/main/webapp/js/viewer.min.js'; // Replace your path to the viewer js, e.g. https://www.example.com/js/viewer.min.js
+window.DRAWIO_LIGHTBOX_URL = 'https://arw-wk.github.io/abd/src/main/webapp/'; // Replace with your lightbox URL, eg. https://www.example.com
+window.DRAW_MATH_URL = 'math/es5';
+window.DRAWIO_CONFIG = {'defaultLibraries':''}; // Replace with your custom draw.io configurations. For more details, https://www.diagrams.net/doc/faq/configure-diagram-editor
+urlParams['sync'] = 'auto';
+urlParams['clibs'] = 'Uhttps%3A%2F%2Fraw.githubusercontent.com%2Farw-wk%2Fabd-custom%2Fmain%2Fbd_library.xml'
+urlParams['create'] = 'Uhttps%3A%2F%2Fraw.githubusercontent.com%2Farw-wk%2Fabd-custom%2Fmain%2Fbd_template.drawio.xml'
+//urlParams['libs'] = '%02%03'; // Required for defaultLlibraries above to work
