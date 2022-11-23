96bcd825f917c9c05d72ed9073ce56fbee66ae77
arw-wk <118310911+arw-wk@users.noreply.github.com>
2022-11-23 15:31:48 -0800

    Update plugintest.js

--- plugintest.js
+++ plugintest.js
@@ -5,6 +5,20 @@ Draw.loadPlugin(function(ui)
 {
 	// Adds resource for action
 	mxResources.parse('exploreFromHere=Explore from here...');
+	mxResources.parse('blockTop=Block')
+	mxResources.parse('blockSubMCU=MCU')
+	mxResources.parse('blockSubMCU=Memory')
+	mxResources.parse('blockSubMCU=Power')
+	mxResources.parse('blockSubMCU=Protection')
+	
+	mxResources.parse('blockSubSubMCU=DC-DC')
+	mxResources.parse('blockSubSubMCU=AC-DC')
+	mxResources.parse('blockSubSubMCU=Modulee')
+	mxResources.parse('blockSubSubMCU=Processor')
+	mxResources.parse('blockSubSubMCU=SOM')
+	mxResources.parse('blockSubSubMCU=SBC')
+	mxResources.parse('blockSubSubMCU=TVS')
+	mxResources.parse('blockSubSubMCU=ESD')
 	
 	// Max number of edges per page
 	var pageSize = 20;
@@ -19,6 +33,8 @@ Draw.loadPlugin(function(ui)
 		if (graph.model.isVertex(graph.getSelectionCell()))
 		{
 			this.addMenuItems(menu, ['-', 'exploreFromHere'], null, evt);
+			this.addMenuItems(menu, ['-', 'blockTop'], null, evt);
+			this.addMenuItems(menu, ['-', 'blockTop', '-','MCU'], null, evt);
 		}
 	};
 
